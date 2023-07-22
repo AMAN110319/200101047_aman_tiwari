67f737e0014d5f5ea7886ca25a3af4ded2ad91e9
Aman Tiwari <98185335+AMAN110319@users.noreply.github.com>
2023-07-22 23:44:09 +0530

    frontend_commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,21 @@
-// src/App.js
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Corrected import statement
-import axios from 'axios';
-import AllTrains from './pages/AllTrains';
-import SingleTrain from './pages/SingleTrain';
-
-const App = () => {
-  const [allTrains, setAllTrains] = useState([]);
-
-  useEffect(() => {
-    // Fetch all trains from the backend API
-    const fetchAllTrains = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/trains');
-        setAllTrains(response.data);
-      } catch (error) {
-        console.error('Error fetching all trains:', error.message);
-      }
-    };
-
-    fetchAllTrains();
-  }, []);
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Home from "./pages/Home.js";
+import SearchTrain from "./pages/Search.js";
+ 
+const App=()=>{
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">All Trains</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes> {/* Use Routes instead of Switch */}
-          <Route path="/" element={<AllTrains trains={allTrains} />} />
-          <Route path="/trains/:trainNumber" element={<SingleTrain />} />
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/search" element={<SearchTrain/>}/>
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </> 
   );
-};
+}
 
 export default App;
